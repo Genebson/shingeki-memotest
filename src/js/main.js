259d38a7f249d17bd6c8bfc0d63d2cc60a32d1bc
Genebson <mauryavero@gmail.com>
2021-06-07 21:12:41 -0300

    Corrige tamaño de las cartas y funcionalidades

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,18 @@ const $form = document.querySelector('#form')
 
 let secuenciaMaquina = []
 let secuenciaUsuario = []
+let intentos = 0
+
+const cardsFront = [
+  'src/imgs/cards/armin.jpg',
+  'src/imgs/cards/eren.jpg',
+  'src/imgs/cards/erwin.jpg',
+  'src/imgs/cards/historia.jpg',
+  'src/imgs/cards/levi.png',
+  'src/imgs/cards/mikasa.jpg',
+  'src/imgs/cards/reiner.jpg',
+  'src/imgs/cards/sasha.jpg'
+]
 
 const resetSound = document.querySelector('#reset');
 const resetSoundOption = document.querySelector('#reset-option');
@@ -12,24 +24,62 @@ const screamMikasaSound = document.querySelector('#scream-mikasa');
 const cardFlipSound = document.querySelector('#card-flip');
 
 $form.boton.onclick = function () {
+  mensajeAutomatico('¡Comencemos! 🔮')
   resetSoundOption.play()
   resetSoundOption.volume = 0.6
   ocultarBoton()
 }
 
-// function bababa() {
-//   const imagenes = document.querySelectorAll('.card-front')
-//   imagenes.forEach(function (imagen) {
-//     imagen.src = "/src/imgs/cards/card-backwards.png"
-//   })
+// $cards.onclick = function () {
+//   // $cards.classList.add('card-back')
+//   voltearCartas()
+//   cardFlipSound.play()
+//   cartaRandom()
+//   bloquearInputUsuario()
 // }
 
+const $cards = document.querySelectorAll('.cards-container')
+$cards.forEach(function ($card) {
+  $card.onclick = function () {
+    cartaRandom()
+    cardFlipSound.play()
+    voltearCartas()
+    // bloquearInputUsuario()
+  }
+})
+
+function voltearCartas(e) {
+  const $imagenes = document.querySelector('.cards-container')
+
+  $imagenes.forEach(function ($imagen) {
+    if (e.target.classList.contains('cards'))
+      $imagen.classList.add('card-back')
+  })
+}
+
+function cartaRandom() {
+  const $imagenes = document.querySelectorAll('.cards')
+  let number = Math.floor(Math.random() * (cardsFront.length))
+
+  $imagenes.forEach(function ($imagen) {
+    $imagen.src = cardsFront[number]
+  })
+}
+
+function compararCartas() {
+
+}
+
+function intentosMaximos(intentos) {
+  document.querySelector('#max-intentos').value = intentos
+}
 
 function bloquearInputUsuario() {
   const $cards = document.querySelectorAll('.cards')
 
   $cards.forEach(function ($card) {
     $card.onclick = function () {
+      console.log('block');
     }
   })
 }
@@ -40,6 +90,67 @@ function desbloquearInputUsuario() {
   $cards.forEach(function ($card) {
     $card.onclick = manejarInputUsuario()
   })
+}
+
+function mensajeAutomatico(mensaje) {
+  const $p = document.querySelector('#mensaje')
+  $p.innerText = mensaje
+}
+
+function mensajeCartaErronea() {
+  const frases = [
+    'Nop, casi 🙄',
+    'Ooooooole 😂',
+    'Dale, vos podes 🙃',
+    'Esa no era, pero está cerca 🤫',
+    '',
+    '',
+  ]
+
+  const fraseAleatoria = Math.floor(Math.random() * (frases.length - 1))
+  return frases[fraseAleatoria]
+}
+
+function mensajeCartaCorrecta() {
+  const frases = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]
+
+  const fraseAleatoria = Math.floor(Math.random() * (frases.length - 1))
+  return frases[fraseAleatoria]
+}
+
+function mensajePerdedor() {
+  const frases = [
+    'Te quedaste sin intentos 😪',
+    'Perdiste 😞',
+    'Perdiste pero la próxima es tuya, dale 😉',
+    'Nooooo 😢',
+    'Se acabó el tiempo ⌛',
+    'Intentá otra vez 🥺',
+  ]
+
+  const fraseAleatoria = Math.floor(Math.random() * (frases.length - 1))
+  return frases[fraseAleatoria]
+}
+
+function mensajeGanador() {
+  const frases = [
+    '',
+    '',
+    '',
+    '',
+    '',
+    '',
+  ]
+
+  const fraseAleatoria = Math.floor(Math.random() * (frases.length - 1))
+  return frases[fraseAleatoria]
 }
 
 function mostrarBoton() {
